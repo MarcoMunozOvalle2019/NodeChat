b710ec3372df728114bc3358d90c2f507a0514c3
MarcoMunozOvalle2019e <marco.munoz.ovalle2010@gmaile.com>
2023-02-07 12:48:56 -0300

    fix: all

--- index.js
+++ index.js
@@ -1,14 +1,43 @@
+//en url delphi
+//http://localhost:3000/act2/J1:Stock;J2:Stock%20Sale;J3:0035719061;N1:marco;N2:manuel;N3:heriberto;
+
 const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const port = process.env.PORT || 3000;
+const path = require('path');
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
+app.get('/act/:id1', async(req, res) => {
+  let data=req.params.id1
+  //io.emit('chat message', data);
+  console.log('pidio:', data)
+//  res.json({marco:14})
+  let leads = ['123456',data]
+  res.render('leads', {leads});  
+});
+
+app.get('/act2/:id1', async(req, res) => {
+  console.log('be',req.params)
+  let data=req.params.id1
+  io.emit('chat message', data);
+  res.json({envie:11})
+  //res.json({envie:data})
+
+
+//  let leads = ['123456',data]
+//  res.render('leads1', {leads});  
+});
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+
 io.on('connection', (socket) => {
   socket.on('chat message', msg => {
+    console.log('uuu','fffffff')
     io.emit('chat message', msg);
   });
 });
